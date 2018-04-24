be1df0380a3c2b6263c05cb95c863ac37192ced5
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-04-24 13:14:51 +0900

    temp

---
+++ src/Test1.ts
@@ -0,0 +1,28 @@
+
+enum LocalEnum {
+    A,
+    B,
+}
+
+// declare global {
+//     enum GlobalEnum {
+//         A,
+//         B,
+//     }
+// }
+
+
+// declare global {
+//    }
+
+let a: StructureConstant
+
+interface StructureSpawn {
+    fuga():void
+  }
+
+
+// StructureSpawn.prototype.fuga = function(){
+//     console.log('This is extended method fuga')
+//   }
+
--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,56 @@ export const loop = ErrorMapper.wrapLoop(() => {
     }
   }
 });
+
+enum ActionResult {
+  OK = 'ok'
+}
+
+interface GroupActionType {
+  say(): ActionResult
+  expand(roomnames: string[]): ActionResult
+}
+
+let a = new Map<string, StructureSpawn>()
+
+class Empire implements GroupActionType {
+  constructor(readonly spawns: Map<string, StructureSpawn>) {
+  }
+
+  say(): ActionResult {
+    let results: ActionResult[] = []
+
+    this.spawns.forEach((spawn, spawnID) => {
+      results.push(spawn.say())
+    })
+
+    return ActionResult.OK
+  }
+
+  expand(roomnames: string[]): ActionResult {
+    console.log('Empire.expand() not implemented yet')
+    return ActionResult.OK
+  }
+}
+
+declare global {
+  interface StructureSpawn extends GroupActionType {
+    creeps: Map<string, Creep>
+
+    initialize(): void
+  }
+}
+
+StructureSpawn.prototype.initialize = function() {
+  this.creeps = Game.creeps.values()
+}
+
+StructureSpawn.prototype.say = function() {
+  console.log('StructureSpawn.say() not implemented yet')
+  return ActionResult.OK
+}
+
+StructureSpawn.prototype.expand = function(roomnames: string[]) {
+  console.log('StructureSpawn.expand() not implemented yet')
+  return ActionResult.OK
+}
