63b16824cb57fb91515bf1316d3d9c7c0fb1f6bf
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-05-17 00:56:39 +0900

    AttackerSquad creates as many attacker as possible when the base is under attack

--- src/classes/squad/attacker.ts
+++ src/classes/squad/attacker.ts
@@ -3,26 +3,25 @@ import { Squad, SquadType, SquadMemory, SpawnPriority, SpawnFunction } from "./s
 import { CreepStatus, ActionResult, CreepType } from "classes/creep"
 
 export class AttackerSquad extends Squad {
-  private attacker: Creep | undefined
   private destination: Room | undefined
   private energy_unit = 330
   private fix_part_energy = 120
-  private max_energy = 1600
+  private max_energy = 1930
 
-  constructor(readonly name: string, readonly rooms_to_defend: Room[], readonly room_for_wait: Room) {
+  constructor(readonly name: string, readonly rooms_to_defend: Room[], readonly base_room: Room) {
     super(name)
 
-    this.creeps.forEach((creep) => {
-      if (creep.memory.type != CreepType.ATTACKER) {
-        return
-      }
-      this.attacker = creep
-    })
+    if ((this.rooms_to_defend.indexOf(this.base_room) >= 0)) {
+      this.destination = this.base_room
+    }
+    else {
+      this.destination = rooms_to_defend[0]
+    }
 
-    this.destination = rooms_to_defend[0]
+    const attacker = Array.from(this.creeps.values())[0]
 
     if (this.rooms_to_defend.length > 0) {
-      const attacker_description = !(!this.attacker) ? `${this.attacker!}, ${this.attacker!.pos}` : ''
+      const attacker_description = attacker ? `${attacker.name}, ${attacker.pos}` : ''
       console.log(`Room Attacked!! ${this.rooms_to_defend}, ${attacker_description}`)
     }
   }
@@ -41,8 +40,13 @@ export class AttackerSquad extends Squad {
 
   // --
   public get spawnPriority(): SpawnPriority {
-    const needs_attacker = (!this.attacker) && (this.rooms_to_defend.length > 0)
-    return needs_attacker ? SpawnPriority.URGENT : SpawnPriority.NONE
+    if (!this.destination) {
+      return SpawnPriority.NONE
+    }
+    if (this.base_room.name == this.destination.name) {
+      return SpawnPriority.URGENT
+    }
+    return this.creeps.size > 0 ? SpawnPriority.NONE : SpawnPriority.URGENT
   }
 
   public hasEnoughEnergy(energyAvailable: number, capacity: number): boolean {
@@ -86,67 +90,66 @@ export class AttackerSquad extends Squad {
   }
 
   public run(): void {
-    const attacker = this.attacker
-    if (!attacker) {
-      return
-    }
-    if (!this.destination) {
-      if (attacker.moveToRoom(this.room_for_wait.name) == ActionResult.DONE) {
-        switch (attacker.room.name) {
-          case 'W48S47':
-            attacker.moveTo(39, 27)
-            break
+    this.creeps.forEach((attacker) => {
+      if (!this.destination) {
+        if (attacker.moveToRoom(this.base_room.name) == ActionResult.DONE) {
+          switch (attacker.room.name) {
+            case 'W48S47':
+              attacker.moveTo(39, 27)
+              break
 
-          case 'W49S47':
-            attacker.moveTo(17, 26)
-            break
+            case 'W49S47':
+              attacker.moveTo(17, 26)
+              break
 
-          case 'W44S42':
-            attacker.moveTo(11, 28)
-            break
+            case 'W44S42':
+              attacker.moveTo(11, 28)
+              break
 
-          default:
-            console.log(`Attacker unexpected waiting room ${attacker.room}, ${attacker.name}, ${this.name}`)
-            break
+            default:
+              console.log(`Attacker unexpected waiting room ${attacker.room}, ${attacker.name}, ${this.name}`)
+              break
+          }
         }
+        return
       }
-      return
-    }
 
-    // const hostile_creep: Creep = attacker.pos.findClosestByPath(FIND_HOSTILE_CREEPS)
-    // if (hostile_creep) {
-    //   if (Game.time % 5) {
-    //     attacker.say('FOUND YOU', true)
-    //   }
+      // const hostile_creep: Creep = attacker.pos.findClosestByPath(FIND_HOSTILE_CREEPS)
+      // if (hostile_creep) {
+      //   if (Game.time % 5) {
+      //     attacker.say('FOUND YOU', true)
+      //   }
 
-    //   const rr = attacker.rangedAttack(hostile_creep)
-    //   if (rr == ERR_NOT_IN_RANGE) {
-    //     const r = attacker.moveTo(hostile_creep)
-    //     // console.log(`FUGA ${attacker}, ${r}, ${hostile_creep}, ${hostile_creep.pos}`)
-    //   }
-    //   // console.log(`HOGE ${attacker}, ${rr}, ${hostile_creep}, ${hostile_creep.pos}`)
-    //   return
-    // }
+      //   const rr = attacker.rangedAttack(hostile_creep)
+      //   if (rr == ERR_NOT_IN_RANGE) {
+      //     const r = attacker.moveTo(hostile_creep)
+      //     // console.log(`FUGA ${attacker}, ${r}, ${hostile_creep}, ${hostile_creep.pos}`)
+      //   }
+      //   // console.log(`HOGE ${attacker}, ${rr}, ${hostile_creep}, ${hostile_creep.pos}`)
+      //   return
+      // }
 
-    // if (!this.destination) {
-    //   // console.log(`Attacker wait ${attacker!.name}, ${this.name}`)
-    //   // if (attacker!.moveToRoom(this.room_for_wait.name) == ActionResult.IN_PROGRESS) {
-    //   //   attacker!.say(this.room_for_wait.name)
-    //   // }
-    //   return
-    // }
+      // if (!this.destination) {
+      //   // console.log(`Attacker wait ${attacker!.name}, ${this.name}`)
+      //   // if (attacker!.moveToRoom(this.room_for_wait.name) == ActionResult.IN_PROGRESS) {
+      //   //   attacker!.say(this.room_for_wait.name)
+      //   // }
+      //   return
+      // }
 
-    attacker.searchAndDestroy()
+      attacker.searchAndDestroy()
 
-    const room = this.destination
-    if (attacker.moveToRoom(room.name) != ActionResult.DONE) {
-      attacker.say(room.name)
-      return
-    }
+      const room = this.destination
+      if (attacker.moveToRoom(room.name) != ActionResult.DONE) {
+        attacker.say(room.name)
+        return
+      }
+    })
   }
 
   public description(): string {
-    const attacker_info = this.attacker ? `${this.attacker.name} ${this.attacker.pos}` : ''
+    const attacker = Array.from(this.creeps.values())[0]
+    const attacker_info = attacker ? `${attacker.name} ${attacker.pos}` : ''
     return `${super.description()}, ${attacker_info}\n    - to ${this.destination} (${this.rooms_to_defend})`
   }
 
