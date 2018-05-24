a5a90f44b4358083a2f675f49006798db29208cf
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-05-24 13:00:58 +0900

    Add harvester source

--- src/classes/region.ts
+++ src/classes/region.ts
@@ -158,10 +158,11 @@ export class Region {
         lightweight_harvester_targets = [
           { id: '59f19ff082100e1594f35c80', room_name: 'W49S46' },  // top
           { id: '59f19fff82100e1594f35e04', room_name: 'W48S46' },  // top right
+          { id: '59f19ff082100e1594f35c7e', room_name: 'W49S45' },  // top top
         ]
-        rooms_need_to_be_defended = ['W49S46', 'W48S46']
+        rooms_need_to_be_defended = ['W49S46', 'W48S46', 'W49S45']
         this.room_names = [this.room.name, 'W49S48', 'W49S49'] //, 'W49S48']//, 'W49S46', 'W48S46']
-        rooms_need_scout = ['W49S46', 'W48S46', 'W47S45']
+        rooms_need_scout = ['W49S46', 'W48S46', 'W47S45', 'W49S45']
         upgrader_source_ids = ['5aef62f86627413133777bdf']
         research_input_targets = [
           {
